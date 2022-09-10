a016efa876a660f663f9c6f556a684af1b5647c3
sanomaru3 <sakiyonezu1221@gmail.com>
2022-09-10 15:21:26 +0900

    list data sample.

--- main.py
+++ main.py
@@ -2,4 +2,36 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title('戸建て賃貸需要供給調査ツール!')
+st.title('戸建て賃貸需要供給調査ツール')
+
+df = pd.DataFrame({
+    '1列目':[1,2,3,4],
+    '2列目':[10,20,30,40]
+})
+
+dict = {'東京都':['練馬区', '大泉町'],
+       '神奈川県':['平塚市']}
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    ken = st.selectbox(
+        '都道府県を選択ください',
+        #('sanomaru', 'ityan', 'utamaru')
+        tuple(dict.keys())
+    )
+
+with col2:
+    city = st.selectbox(
+        '市区町村を選択してください',
+        #('sanomaru2', 'ityan2')
+        tuple(dict[ken])
+    )
+    #st.write('city: ', city)
+
+with col3:
+    if st.button('GO!'):
+        st.write('your choice : ', ken, city)
+    else:
+        st.write('push start')
+
