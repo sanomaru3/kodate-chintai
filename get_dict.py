6d0cdff785b21f64eaa92c86d0d72946f806a05d
sanomaru3 <sakiyonezu1221@gmail.com>
2022-09-11 17:44:49 +0900

    selectbox data change

---
+++ get_dict.py
@@ -0,0 +1,34 @@
+import os
+import csv
+
+def get_list(filename):
+
+    list = []
+    
+    # csv 読込み
+    csvfile = open(filename, 'r')
+    datas = csv.reader(csvfile)
+
+    for data in datas:
+        list.append(data[0])
+
+    return list
+
+def get_dict():
+    dict = {}
+
+    # file name 取得
+    dir = "./japan"
+    files = os.listdir(dir)
+
+    for file in files:
+        rf = './japan/' + file
+
+        # file名抽出
+        ken = file.replace('.csv', '')
+
+        # dict追加
+        dict[ken] = get_list(rf)
+    
+    return dict
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import get_dict as gd
+
+japan = gd.get_dict()
+#st.write(japan["東京都"])
 
 st.title('戸建て賃貸需要供給調査ツール')
 
@@ -18,14 +22,16 @@ with col1:
     ken = st.selectbox(
         '都道府県を選択ください',
         #('sanomaru', 'ityan', 'utamaru')
-        tuple(dict.keys())
+        #tuple(dict.keys())
+        tuple(japan.keys())
     )
 
 with col2:
     city = st.selectbox(
         '市区町村を選択してください',
         #('sanomaru2', 'ityan2')
-        tuple(dict[ken])
+        #tuple(dict[ken])
+        tuple(japan[ken])
     )
     #st.write('city: ', city)
 
