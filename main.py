97c8ab699c9f039cef65e8e755056e89c8525663
sanomaru3 <sakiyonezu1221@gmail.com>
2022-10-09 18:19:17 +0900

    add csv file download link

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import get_dict as gd
 import get_result_dic_list as gr
+import base64
 
 show_flg = False
 
@@ -63,3 +64,11 @@ if(show_flg):
 
 
     #st.write('city: ', city)
+
+    # CSVのダウンロードリンクを生成
+    csv = df.to_csv(index=False)  
+
+    # utf-8(BOM)
+    b64 = base64.b64encode(csv.encode('utf-8-sig')).decode()
+    href = f'<a href="data:application/octet-stream;base64,{b64}" download="result_utf-8-sig.csv">Download Link</a>'
+    st.markdown(f"CSVファイルのダウンロード(utf-8 BOM):  {href}", unsafe_allow_html=True)
